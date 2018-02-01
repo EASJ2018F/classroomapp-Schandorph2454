db93994511b0d54d26f0682acfde162d5b8c5bf8
Linda Lene Schandorph Larsen <lind6181@edu.easj.dk>
2018-02-01 16:11:23 +0100

    Opgave 6 lavet

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class KlasseRum
+    {
+        public string _klasseNavn;
+        public List<Studerende> Klasseliste;
+        public DateTime _semesterStart;
+
+        public KlasseRum(string klasseNavn, )
+        {
+            _klasseNavn = 
+        }
+    }
+}
