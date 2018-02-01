3634e324f35d3a6c92a1c2f32fe0db19c512059a
Linda Lene Schandorph Larsen <lind6181@edu.easj.dk>
2018-02-01 15:26:28 +0100

    Opgave 1 fuldført

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Studerende
+    {
+        private string _navn;
+        private int _fødselsmåned;
+        private int _fødselsdag;
+
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            _navn = navn;
+            _fødselsmåned = fødselsmåned;
+            _fødselsdag = fødselsdag;
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value;}
+        }
+
+        public int fødselsmåned
+        {
+            get { return _fødselsmåned; }
+            set { _fødselsmåned = value;}
+        }
+
+        public int fødselsdag
+        {
+            get { return _fødselsdag; }
+            set { _fødselsdag = value;}
+        }
+    }
+}
